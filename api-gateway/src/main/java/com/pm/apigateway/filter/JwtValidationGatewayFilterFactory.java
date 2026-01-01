8683efe9e10680acd24f4d76e9a5c41b7988bcb0
msinha569 <msinha569@gmail.com>
2026-01-02 01:29:03 +0530

    auth jwt implemented, api-gateway

---
+++ api-gateway/src/main/java/com/pm/apigateway/filter/JwtValidationGatewayFilterFactory.java
@@ -0,0 +1,35 @@
+package com.pm.apigateway.filter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class JwtValidationGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
+
+    private final WebClient webClient;
+    public JwtValidationGatewayFilterFactory(WebClient.Builder webClientBuilder, @Value("${AUTH_SERVICE_URL}")  String authServiceUri) {
+        this.webClient = webClientBuilder.baseUrl(authServiceUri).build();
+    }
+    @Override
+    public GatewayFilter apply(Object config) {
+       return (exchange, chain) -> {
+           String token = exchange.getRequest().getHeaders().getFirst("Authorization");
+
+           if(token==null || !token.startsWith("Bearer ")) {
+               exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+               return exchange.getResponse().setComplete();
+           }
+           return webClient.get()
+                   .uri("/validate")
+                   .header("Authorization", token)
+                   .retrieve()
+                   .toBodilessEntity()
+                   .then(chain.filter(exchange));
+       };
+    }
+}
---
+++ integrated-tests/src/test/java/AuthIntegrationTest.java
@@ -0,0 +1,2 @@
+public class AuthIntegrationTest {
+}
